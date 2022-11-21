c41331d40427e9aee09d69612f90b545ccd4f391
Mizab <mizabmansari@gmail.com>
2022-11-21 19:23:24 +0530

    created all recipes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { Advancement, advancement, clear, functionCmd, ITEMS, MCFunction, raw, Recipe, say, Selector, SelectorClass } from "sandstone";
 
-let self: SelectorClass<false, true> = Selector('@a');
 function createRecipe(name: string , giveFunctionPath: string, element: ITEMS){
     
+    let self: SelectorClass<false, true> = Selector('@a');
     let thisDatapack: string = "tnt_recipes";
     let tntDatapack: string = "mtnt.main";
 
     MCFunction(name, () => {
         // Takes back the recipe so it can be crafted again
-        say('hiicfg')
         raw(`recipe take ${self} ${thisDatapack}:${name}_recipe`);
         advancement.revoke(self).only(`${thisDatapack}:${name}_adv`);
 
@@ -17,7 +16,7 @@ function createRecipe(name: string , giveFunctionPath: string, element: ITEMS){
 
         // Clear the knowledge book from inventory
         clear(self, 'minecraft:knowledge_book');
-    })
+    });
 
     // advancment for the recipe unlock, give the TNT to the owner
     Advancement(`${name}_adv`, {
@@ -32,7 +31,7 @@ function createRecipe(name: string , giveFunctionPath: string, element: ITEMS){
         "rewards": {
             "function": `${thisDatapack}:${name}`
         }
-    })
+    });
 
     // custom recipe for the TNT
     Recipe(`tnt_recipes:${name}_recipe`, {
@@ -57,7 +56,21 @@ function createRecipe(name: string , giveFunctionPath: string, element: ITEMS){
             "item": "minecraft:knowledge_book",
             "count": 1
         }
-    })
+    });
 }
 
-createRecipe("effect_tnt", "good_effect", "minecraft:glass_bottle");
+createRecipe("effect_tnt", "good_effect", "minecraft:glass_bottle"); // ✅
+createRecipe("fish_tnt", "fish", "minecraft:tropical_fish"); // ✅
+createRecipe("firework_tnt", "missile", "minecraft:firework_rocket"); // ✅
+createRecipe("recursion_tnt", "recursion", "minecraft:tnt"); // ✅
+createRecipe("sponge_tnt", "sponge", "minecraft:sponge"); // ✅
+createRecipe("butcher_tnt", "butcher", "minecraft:fermented_spider_eye"); // ✅
+createRecipe("rich_tnt", "rich", "minecraft:emerald"); // ✅
+createRecipe("lightning_tnt", "lightning", "minecraft:lightning_rod"); // ✅
+createRecipe("hostile_tnt", "hostile", "minecraft:bone"); // ✅
+createRecipe("drill_tnt", "drill", "minecraft:iron_pickaxe"); // ✅
+createRecipe('tsunami_tnt', "tsunami", "minecraft:water_bucket"); // ✅
+createRecipe("wwz_tnt", "wwz", "minecraft:rotten_flesh"); // ✅
+createRecipe("inverted_tnt", "inverted", "minecraft:name_tag"); // ✅
+createRecipe("farm_tnt", "farm", "minecraft:beef"); // ✅
+createRecipe("freeze_tnt", "ice", "minecraft:ice"); // ✅
